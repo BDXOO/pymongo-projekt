1f83c78e961a06e1f060edd65cde4a02497c6928
Bartosz Dembicki <bartoszdembicki@gmail.com>
2025-04-30 11:35:03 +0200

    base commit

---
+++ layout.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+class Layout:
+    def main() -> None:
+        pass
+    
+    def insert() -> None:
+        pass
+    
+    def delete() -> None:
+        pass
+    
+    def find() -> None:
+        pass
+    
+    def update() -> None:
+        pass
+    
+    def requirements(requirements:dict[str:str]) -> dict[str:str]:
+        pass
